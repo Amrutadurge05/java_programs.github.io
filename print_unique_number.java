d4993a256078c144e4267c8e1ac37cad1784db47
Amrutadurge05 <162823712+Amrutadurge05@users.noreply.github.com>
2024-04-02 22:05:54 +0530

    Add files via upload

---
+++ print_unique_number.java
@@ -0,0 +1,30 @@
+import java.io.*;
+ 
+class print_unique_number {
+ 
+    static int countDistinct(int arr[], int n)
+    {
+        int res = 1;
+ 
+        
+        for (int i = 1; i < n; i++) {
+            int j = 0;
+            for (j = 0; j < i; j++)
+                if (arr[i] == arr[j])
+                    break;
+ 
+            if (i == j)
+                res++;
+        }
+        return res;
+    }
+ 
+    
+    public static void main(String[] args)
+    {
+        int arr[] = { 12, 10, 9, 45, 2, 10, 10, 45 };
+        int n = arr.length;
+        System.out.println(countDistinct(arr, n));
+    }
+}
+ 
